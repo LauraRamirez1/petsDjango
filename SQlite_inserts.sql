885df9937b3ec59739e462db4fd3598fb65fc43e
Jacqueline Galimany <42303655+jgalsku@users.noreply.github.com>
2023-05-09 00:35:55 -0700

    sql inserts

---
+++ SQlite_inserts.sql
@@ -0,0 +1,22 @@
+-- SQLite
+
+DELETE FROM core_mascota;
+DELETE FROM core_mascota;
+DELETE FROM core_mascota;
+DELETE FROM core_mascota;
+
+
+INSERT INTO core_cliente (nombre, edadMascota, razaMascota, nombre_id) 
+VALUES ('Jack', 12, 'daschund', 3);
+
+
+INSERT INTO core_mascota (nombreMascota, edadMascota, razaMascota, nombre_id) 
+VALUES ('Jack', 12, 'daschund', 3);
+
+
+INSERT INTO core_producto (nombreProducto, precioProducto, descripcionProducto) 
+VALUES ('Furminator', 12990, 'Peine para perro');
+
+
+INSERT INTO core_servicio (nombreProducto, precioProducto, descripcionProducto) 
+VALUES ('Furminator', 12990, 'Peine para perro');
